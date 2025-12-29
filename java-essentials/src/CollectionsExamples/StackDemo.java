b9d97bab6d4d0f6a17df2d579777ad6ce3f0fa5f
TS1A0103 <vamsi.krishna@mail.techsemble.us>
2025-12-29 12:19:49 -0600

    stack example

---
+++ java-essentials/src/CollectionsExamples/StackDemo.java
@@ -0,0 +1,51 @@
+package CollectionsExamples;
+import java.util.Stack;
+import java.util.List;
+public class StackDemo {
+
+    public static int add(int n){
+        if(n<=0){
+            return 0;
+        }
+
+        int sum = add(n-1);
+        return(n+sum);
+    }
+
+
+    public static void main(String[] args){
+        int n = 10;
+        int sum = add(n);
+        System.out.println(sum);
+
+        //Declaration
+        Stack<Integer> stack1 = new Stack<>();
+//
+        //Inserion of elements into stack
+        stack1.push(10);
+        stack1.push(20);
+        stack1.push(30);
+        stack1.push(40);
+        stack1.push(50);
+
+        System.out.println(stack1);  //[10,20,30,40,50]
+        System.out.println("-----");
+        System.out.println(stack1.pop());//50
+        System.out.println("-----");
+        System.out.println(stack1); //[10,20,30,40]
+        System.out.println("-----");
+        System.out.println(stack1.peek());
+        System.out.println("-----");
+        System.out.println(stack1);
+        stack1.remove(1);
+        System.out.println("-----");
+        System.out.println(stack1);
+        System.out.println("-----");
+        stack1.push(20);
+        System.out.println(stack1);
+        System.out.println("-----");
+        System.out.println(stack1.search(20));
+        System.out.println("-----");
+        System.out.println(stack1);
+    }
+}
