af754fb0304b50a1906c73373385b6bf2acc688b
TS1A0103 <vamsi.krishna@mail.techsemble.us>
2025-12-31 03:48:19 -0600

    Bubble sorting Demo

---
+++ java-essentials/src/DSAConcepts/BubbleSortingDemo.java
@@ -0,0 +1,40 @@
+package DSAConcepts;
+
+public class BubbleSortingDemo {
+    public static void main(String[] args){
+        int temp;
+        int[] nums = new int[7];
+        nums[0] = 2;
+        nums[1] = 9;
+        nums[2] = 6;
+        nums[3] = 5;
+        nums[4] = 24;
+        nums[5] = 21;
+        nums[6] = 0;
+        System.out.println("Array before sorting");
+        for (int num : nums) {
+            System.out.print(num + " "); //2 9 6 5 24 21 0
+        }
+        for(int i=0; i<nums.length; i++){
+            for(int j= 0; j<nums.length-i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+            System.out.println();
+            for (int num : nums) {
+                System.out.print(num + " ");//Variable array
+            }
+            System.out.println();
+
+        }
+
+        System.out.println();
+        System.out.println("Array after sorting");
+        for (int num : nums) {
+            System.out.print(num + " ");//2 5 6 9 21 24
+        }
+    }
+}
